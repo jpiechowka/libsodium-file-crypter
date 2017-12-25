fcf898f015e886dafbbf5240b9dbc89d4f5c763f
Janusz Piechówka <januszpiechowka@gmail.com>
2017-12-25 01:09:53 +0100

    Display current time when using logger and displaying logs, align log output

--- utils/logger.c
+++ utils/logger.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
 #include "logger.h"
 
 void log_debug(char *debugMessage) {
-    printf(BLUE "[DEBUG] %s\n" RESET_COLOR, debugMessage);
+    time_t now;
+    time(&now);
+    char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
+    printf(BLUE "[DEBUG][%s] %s\n" RESET_COLOR, ctimeNoNewline, debugMessage);
 }
 
 void log_info(char *infoMessage) {
-    printf("[INFO] %s\n", infoMessage);
+    time_t now;
+    time(&now);
+    char *ctimeNoNewline = strtok(ctime(&now), "\n");
+    printf("[ INFO][%s] %s\n", ctimeNoNewline, infoMessage);
 }
 
 void log_warning(char *warningMessage) {
-    printf(YELLOW "[WARN] %s\n" RESET_COLOR, warningMessage);
+    time_t now;
+    time(&now);
+    char *ctimeNoNewline = strtok(ctime(&now), "\n");
+    printf(YELLOW "[ WARN][%s] %s\n" RESET_COLOR, ctimeNoNewline, warningMessage);
 }
 
 void log_error(char *errorMessage) {
-    printf(RED "[ERROR] %s\n" RESET_COLOR, errorMessage);
+    time_t now;
+    time(&now);
+    char *ctimeNoNewline = strtok(ctime(&now), "\n");
+    printf(RED "[ERROR][%s] %s\n" RESET_COLOR, ctimeNoNewline, errorMessage);
 }
