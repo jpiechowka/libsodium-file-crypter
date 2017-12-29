412f8223a4b15f845743130d3cb58c5e3fe07f2e
Janusz Piechówka <januszpiechowka@gmail.com>
2017-12-29 16:24:55 +0100

    Add option to toggle debug logging on and off

--- utils/logger.c
+++ utils/logger.c
@@ -3,11 +3,24 @@
 #include <string.h>
 #include "logger.h"
 
+struct LoggerOptions {
+    bool showDebugMessages;
+};
+
+struct LoggerOptions loggerOptions = {.showDebugMessages = false};
+
+void toggle_debug_logging(bool shouldDebugLoggingBeEnabled) {
+    loggerOptions.showDebugMessages = shouldDebugLoggingBeEnabled;
+};
+
 void log_debug(char *debugMessage) {
-    time_t now;
-    time(&now);
-    char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
-    printf(BLUE "[DEBUG][%s] %s\n" RESET_COLOR, ctimeNoNewline, debugMessage);
+    // Check if debug logging is enabled
+    if (loggerOptions.showDebugMessages) {
+        time_t now;
+        time(&now);
+        char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
+        printf(BLUE "[DEBUG][%s] %s\n" RESET_COLOR, ctimeNoNewline, debugMessage);
+    }
 }
 
 void log_info(char *infoMessage) {
--- utils/logger.h
+++ utils/logger.h
@@ -1,11 +1,15 @@
 #ifndef LIBSODIUM_FILE_CRYPTER_LOGGER_H
 #define LIBSODIUM_FILE_CRYPTER_LOGGER_H
 
+#include <stdbool.h>
+
 //Colors used when printing text to console
 #define RED "\x1B[31m"
 #define YELLOW "\x1B[33m"
 #define BLUE "\x1B[34m"
 #define RESET_COLOR "\x1B[0m"
+
+void toggle_debug_logging(bool shouldDebugLoggingBeEnabled);
 
 void log_debug(char *debugMessage);
 
