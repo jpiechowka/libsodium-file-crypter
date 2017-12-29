00cfb7e2bedd68a0aaa71eb10d18434fc9faed95
Janusz Piechówka <januszpiechowka@gmail.com>
2017-12-29 20:54:57 +0100

    Add comments to encrypt_file.h and decrypt_file.h header files about CHUNK_SIZE

--- crypto/decrypt_file.h
+++ crypto/decrypt_file.h
@@ -3,7 +3,7 @@
 
 #include <sodium.h>
 
-#define CHUNK_SIZE 4096
+#define CHUNK_SIZE 4096 // Make sure that CHUNK_SIZE is the same as in the encrypt_file.h
 
 int decrypt_file(const char *fileToDecrypt, const char *outputFile,
                  const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]);
--- crypto/encrypt_file.h
+++ crypto/encrypt_file.h
@@ -3,7 +3,7 @@
 
 #include <sodium.h>
 
-#define CHUNK_SIZE 4096
+#define CHUNK_SIZE 4096 // Make sure that CHUNK_SIZE is the same as in the decrypt_file.h
 
 int encrypt_file(const char *fileToEncrypt, const char *outputFile,
                  const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]);
