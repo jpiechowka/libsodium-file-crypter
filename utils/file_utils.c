76455be76171f0c7e107ac6d89ce8c700a6c6acd
Janusz Piechówka <januszpiechowka@gmail.com>
2017-12-31 00:58:00 +0100

    Delete logging to prevent producing unnecessary logs as file_utils functions are used in different places

--- utils/file_utils.c
+++ utils/file_utils.c
@@ -5,11 +5,9 @@
 bool check_if_file_exists(const char *filePath) {
     if (access(filePath, F_OK) != -1) {
         // File exists
-        log_debug_concat_str(filePath, " file exists");
         return true;
     } else {
         // File doesn't exist
-        log_error_concat_str(filePath, " file does not exists");
         return false;
     }
 }
@@ -17,11 +15,9 @@ bool check_if_file_exists(const char *filePath) {
 bool check_if_file_is_readable(const char *filePath) {
     if (access(filePath, R_OK) != -1) {
         // File readable
-        log_debug_concat_str(filePath, " file is readable");
         return true;
     } else {
         // File is not readable
-        log_error_concat_str(filePath, " file is not readable");
         return false;
     }
 }
@@ -29,11 +25,9 @@ bool check_if_file_is_readable(const char *filePath) {
 bool check_if_file_is_writable(const char *filePath) {
     if (access(filePath, W_OK) != -1) {
         // File is writable
-        log_debug_concat_str(filePath, " file is writable");
         return true;
     } else {
         // File is not writable
-        log_error_concat_str(filePath, " file is not writable");
         return false;
     }
 }
