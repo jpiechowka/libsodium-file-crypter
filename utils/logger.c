e61761b1b44e7d07a2ce9d7a7a263913d6e3e44f
Janusz Piechówka <januszpiechowka@gmail.com>
2017-12-30 23:08:00 +0100

    Add additional logging function that allow displaying two concatenated strings. Functions use strncpy and strncat to protect against buffer overflows

--- utils/logger.c
+++ utils/logger.c
@@ -13,7 +13,7 @@ void toggle_debug_logging(bool shouldDebugLoggingBeEnabled) {
     loggerOptions.showDebugMessages = shouldDebugLoggingBeEnabled;
 };
 
-void log_debug(char *debugMessage) {
+void log_debug(const char *debugMessage) {
     // Check if debug logging is enabled
     if (loggerOptions.showDebugMessages) {
         time_t now;
@@ -23,23 +23,70 @@ void log_debug(char *debugMessage) {
     }
 }
 
-void log_info(char *infoMessage) {
+void log_debug_concat_str(const char *debugMessage, const char *stringToConcatenate) {
+    char concatenatedLog[CONCAT_STR_BUFFER_LEN];
+    strncpy(concatenatedLog, debugMessage, sizeof(concatenatedLog));
+    strncat(concatenatedLog, stringToConcatenate, sizeof(concatenatedLog));
+
+    // Check if debug logging is enabled
+    if (loggerOptions.showDebugMessages) {
+        time_t now;
+        time(&now);
+        char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
+        printf(BLUE "[DEBUG][%s] %s\n" RESET_COLOR, ctimeNoNewline, concatenatedLog);
+    }
+}
+
+void log_info(const char *infoMessage) {
     time_t now;
     time(&now);
     char *ctimeNoNewline = strtok(ctime(&now), "\n");
     printf("[ INFO][%s] %s\n", ctimeNoNewline, infoMessage);
 }
 
-void log_warning(char *warningMessage) {
+void log_info_concat_str(const char *infoMessage, const char *stringToConcatenate) {
+    char concatenatedLog[CONCAT_STR_BUFFER_LEN];
+    strncpy(concatenatedLog, infoMessage, sizeof(concatenatedLog));
+    strncat(concatenatedLog, stringToConcatenate, sizeof(concatenatedLog));
+
     time_t now;
     time(&now);
-    char *ctimeNoNewline = strtok(ctime(&now), "\n");
+    char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
+    printf("[ INFO][%s] %s\n", ctimeNoNewline, concatenatedLog);
+}
+
+void log_warning(const char *warningMessage) {
+    time_t now;
+    time(&now);
+    char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
     printf(YELLOW "[ WARN][%s] %s\n" RESET_COLOR, ctimeNoNewline, warningMessage);
 }
 
-void log_error(char *errorMessage) {
+void log_warning_concat_str(const char *warningMessage, const char *stringToConcatenate) {
+    char concatenatedLog[CONCAT_STR_BUFFER_LEN];
+    strncpy(concatenatedLog, warningMessage, sizeof(concatenatedLog));
+    strncat(concatenatedLog, stringToConcatenate, sizeof(concatenatedLog));
+
     time_t now;
     time(&now);
-    char *ctimeNoNewline = strtok(ctime(&now), "\n");
+    char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
+    printf(YELLOW "[ WARN][%s] %s\n" RESET_COLOR, ctimeNoNewline, concatenatedLog);
+}
+
+void log_error(const char *errorMessage) {
+    time_t now;
+    time(&now);
+    char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
     printf(RED "[ERROR][%s] %s\n" RESET_COLOR, ctimeNoNewline, errorMessage);
+}
+
+void log_error_concat_str(const char *errorMessage, const char *stringToConcatenate) {
+    char concatenatedLog[CONCAT_STR_BUFFER_LEN];
+    strncpy(concatenatedLog, errorMessage, sizeof(concatenatedLog));
+    strncat(concatenatedLog, stringToConcatenate, sizeof(concatenatedLog));
+
+    time_t now;
+    time(&now);
+    char *ctimeNoNewline = strtok(ctime(&now), "\n"); // Delete newline, so that the log message is not split
+    printf(RED "[ERROR][%s] %s\n" RESET_COLOR, ctimeNoNewline, concatenatedLog);
 }
--- utils/logger.h
+++ utils/logger.h
@@ -3,6 +3,8 @@
 
 #include <stdbool.h>
 
+#define CONCAT_STR_BUFFER_LEN 4096
+
 //Colors used when printing text to console
 #define RED "\x1B[31m"
 #define YELLOW "\x1B[33m"
@@ -11,12 +13,20 @@
 
 void toggle_debug_logging(bool shouldDebugLoggingBeEnabled);
 
-void log_debug(char *debugMessage);
+void log_debug(const char *debugMessage);
 
-void log_info(char *infoMessage);
+void log_debug_concat_str(const char *debugMessage, const char *stringToConcatenate);
 
-void log_warning(char *warningMessage);
+void log_info(const char *infoMessage);
 
-void log_error(char *errorMessage);
+void log_info_concat_str(const char *infoMessage, const char *stringToConcatenate);
+
+void log_warning(const char *warningMessage);
+
+void log_warning_concat_str(const char *warningMessage, const char *stringToConcatenate);
+
+void log_error(const char *errorMessage);
+
+void log_error_concat_str(const char *errorMessage, const char *stringToConcatenate);
 
 #endif //LIBSODIUM_FILE_CRYPTER_LOGGER_H
