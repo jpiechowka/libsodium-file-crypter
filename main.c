eb6d79466640ca48585a67f57801c5d44f34fca5
Janusz Piechówka <januszpiechowka@gmail.com>
2017-12-31 01:43:49 +0100

    Add additional checks when parsing command line arguments passed to program

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <argp.h>
-#include <error.h>
 #include "crypto/decrypt_file.h"
 #include "utils/logger.h"
 #include "crypto/encrypt_file.h"
+#include "utils/file_utils.h"
 
 #define DEFAULT_OUTPUT_FILE "output.crypt"
 
@@ -26,6 +26,7 @@ static struct argp_option options[] = {
         {"verbose", 'v', 0,      0,       "Increase verbosity - show debug messages"},
         {"debug",   0,   0, OPTION_ALIAS, 0},
         {"output",  'o', "FILE", 0,       "Specify output file instead of default: output.crypt"},
+        {"force",   'f', 0,      0,       "Force overwrite of output file if it already exists"},
         {"encrypt", 'e', 0,      0,       "Encrypt the specified file. Specifying operation mode is mandatory for program to run"},
         {"decrypt", 'd', 0,      0,       "Decrypt the specified file. Specifying operation mode is mandatory for program to run"},
         {0}
@@ -35,6 +36,7 @@ static struct argp_option options[] = {
 struct arguments {
     char *args[2]; /* FILE & PASSWORD */
     bool verbose;
+    bool forceOverwrite;
     int operationMode; /* 1 for encryption, 2 for decryption, other values treated as invalid */
     char *outputFile;
 };
@@ -49,6 +51,9 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
             break;
         case 'o':
             arguments->outputFile = arg;
+            break;
+        case 'f':
+            arguments->forceOverwrite = true;
             break;
         case 'd':
             arguments->operationMode = 2;
@@ -83,6 +88,7 @@ int main(int argc, char *argv[]) {
 
     /* Default values */
     arguments.verbose = false;
+    arguments.forceOverwrite = false;
     arguments.operationMode = -1; /* Default is not specified. User needs to provide this value */
     arguments.outputFile = DEFAULT_OUTPUT_FILE;
 
@@ -93,18 +99,47 @@ int main(int argc, char *argv[]) {
         toggle_debug_logging(true);
     }
 
+    /* Check if operation mode is provided by user. If not exit */
+    if (!(arguments.operationMode == 1 || arguments.operationMode == 2)) {
+        log_error(
+                "No operation mode provided. Make sure to use [-d/--decrypt] or [-e/--encrypt] options to specify operation mode");
+        return 1;
+    }
+
+    /* Check if output file exists */
+    if (check_if_file_exists(arguments.outputFile)) {
+        if (arguments.forceOverwrite == false) {
+            log_error_concat_str(arguments.outputFile,
+                                 " exists. Use [-f/--force] option to force overwrite the output file");
+            return 1;
+        } else {
+            log_warning_concat_str("[-f/--force] option used. Overwriting output file: ", arguments.outputFile);
+        }
+    }
+
     /* arguments.args[0] - FILE
      * arguments.args[1] - PASSWORD*/
 
     if (arguments.operationMode == 1) {
-        // Encrypt
+        /* Encryption */
+        /* Check if file to encrypt is not the same as output file*/
+        if (are_two_file_paths_the_same(arguments.args[0], arguments.outputFile) == true) {
+            log_error("File to decrypt and output file are the same. Program cannot operate on the same file");
+            log_error("Specify output file with [-o/--output] option");
+            return 1;
+        }
+
         encrypt_file(arguments.args[0], arguments.outputFile, arguments.args[1]);
     } else if (arguments.operationMode == 2) {
-        //Decrypt
+        /* Decryption */
+        /* Check if file to decrypt is not the same as output file*/
+        if (are_two_file_paths_the_same(arguments.args[0], arguments.outputFile) == true) {
+            log_error("File to decrypt and output file are the same. Program cannot operate on the same file");
+            log_error("Specify output file with [-o/--output] option");
+            return 1;
+        }
+
         decrypt_file(arguments.args[0], arguments.outputFile, arguments.args[1]);
-    } else {
-        log_error("No operation mode provided. Make sure to use [-d] or [-e] options to specify operation mode");
-        return 1;
     }
 
     return 0;
